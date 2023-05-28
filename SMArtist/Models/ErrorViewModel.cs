81eff68abad2d238576c720b5b7c7d82a3f3772a
Paulina Anaya <Paulina Anaya@DESKTOP-VQHCNUK>
2023-05-27 21:22:02 -0700

    Add project files.

---
+++ SMArtist/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SMArtist.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
